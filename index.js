62602b5834f691cd348240e7349ce346ac5b7a8a
volatilev3 <89734410+volatilev3@users.noreply.github.com>
2024-01-11 12:52:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [730, 440, 570, 1817070, 1888930, 1326470, 1174180, 1088850, 1817190, 1938090, 1085660, 239140, 534380, 2195250, 990080];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
